9a9df1d718c887a5eaf14aec5e96e64822974435
Balazs Soltesz <balazs.soltesz@gmail.com>
2023-02-18 15:16:11 +0100

    sumneko_lua -> lsp

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -14,7 +14,7 @@ return {
     opts = {
       servers = {
         jsonls = {},
-        sumneko_lua = {
+        lua_ls = {
           settings = {
             Lua = {
               workspace = {
@@ -139,26 +139,26 @@ return {
   --     })
   --   end,
   -- },
-  {
-    "MunifTanjim/eslint.nvim",
-    opts = {
-      bin = "eslint_d", -- or `eslint_d`
-      code_actions = {
-        enable = true,
-        apply_on_save = {
-          enable = true,
-          types = { "directive", "problem", "suggestion", "layout" },
-        },
-        disable_rule_comment = {
-          enable = true,
-          location = "separate_line", -- or `same_line`
-        },
-      },
-      diagnostics = {
-        enable = true,
-        report_unused_disable_directives = false,
-        run_on = "type", -- or `save`
-      },
-    },
-  },
+  -- {
+  --   "MunifTanjim/eslint.nvim",
+  --   opts = {
+  --     bin = "eslint_d", -- or `eslint_d`
+  --     code_actions = {
+  --       enable = true,
+  --       apply_on_save = {
+  --         enable = true,
+  --         types = { "directive", "problem", "suggestion", "layout" },
+  --       },
+  --       disable_rule_comment = {
+  --         enable = true,
+  --         location = "separate_line", -- or `same_line`
+  --       },
+  --     },
+  --     diagnostics = {
+  --       enable = true,
+  --       report_unused_disable_directives = false,
+  --       run_on = "type", -- or `save`
+  --     },
+  --   },
+  -- },
 }
